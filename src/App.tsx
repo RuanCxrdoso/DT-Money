0e7639c1af99045e8904e6cc50727fc03fdb91a6
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-03-02 23:09:06 -0300

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>DT Money is coming...</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
