0bedcfea5b49ce039c54a6625ff57981fd2f5d59
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-03-05 00:03:02 -0300

    feat: Add modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
-
+import * as Dialog from "@radix-ui/react-dialog"
 import logo from '../../assets/logo.svg'
+
+import { NewTransactionModal } from "../NewTransactionModal";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./style";
 
 export function Header() {
   return (
@@ -8,7 +10,13 @@ export function Header() {
       <HeaderContent>
         <img src={logo} alt="Logo Ignite" />
 
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
