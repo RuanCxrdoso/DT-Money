68fa889326676bcc75ac1c375fb1a267e3ceb1c9
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-03-10 18:46:26 -0300

    feat: Add useCallBack

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode, useEffect, useState, useCallback } from 'react'
 import { api } from '../lib/axios'
 import { createContext } from 'use-context-selector'
 
@@ -33,7 +33,7 @@ export const TransactionContext = createContext({} as TransactionContextType)
 export function TransactionsProvider({ children }: ChildrenProp) {
   const [transactions, setTransactions] = useState<transactionsType[]>([])
 
-  async function fetchTransactions(query?: string) {
+  const fetchTransactions = useCallback(async (query?: string) => {
     // O json-server não funciona mais assim para receber query params, não encontrei na doc uma forma nova de fazer.
     const response = await api.get('/transactions', {
       params: {
@@ -44,21 +44,24 @@ export function TransactionsProvider({ children }: ChildrenProp) {
     })
 
     setTransactions(response.data)
-  }
+  }, [])
 
-  async function createTransaction(data: CreateTransactionInput) {
-    const { description, price, category, type } = data
+  const createTransaction = useCallback(
+    async (data: CreateTransactionInput) => {
+      const { description, price, category, type } = data
 
-    const response = await api.post('/transactions', {
-      description,
-      price,
-      category,
-      type,
-      createdAt: new Date(),
-    })
+      const response = await api.post('/transactions', {
+        description,
+        price,
+        category,
+        type,
+        createdAt: new Date(),
+      })
 
-    setTransactions((state) => [response.data, ...state])
-  }
+      setTransactions((state) => [response.data, ...state])
+    },
+    [],
+  )
 
   useEffect(() => {
     fetchTransactions()
